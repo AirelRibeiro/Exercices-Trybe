1cda1c9d4040df05dd50e25808c3a93a8337764d
Airel <mv.ribeirosilva@gmail.com>
2022-03-02 17:08:16 -0300

    Nova adiçao de 4.3, finalizaçao dosbonus em revisao de carnaval

--- Fundamentos/Bloco04-IntroducaoJavaScript_LogicadeProgramacao/revisao-bloco-4/script-4-3.js
+++ Fundamentos/Bloco04-IntroducaoJavaScript_LogicadeProgramacao/revisao-bloco-4/script-4-3.js
@@ -1,5 +1,5 @@
 /* Exercício bônus bloco 4, dia 3 
-Foi realizada consulta aos gabaritos, não compreendi como estruturar da questão 2 em diante;
+Foi realizada consulta aos gabaritos:
 Fonte: 
 ==> https://app.betrybe.com/course/fundamentals/introducao-a-javascript-e-logica-de-programacao/javascript-logica-de-programacao-e-algoritmos/solutions/83bb1cd0-be4a-40cf-a9c2-22d71f700144/gabarito-dos-exercicios/dbef6a68-2974-4129-9de4-9031d8d30b94?use_case=calendar
 */
@@ -43,7 +43,7 @@ let inputLine = ''; // Começa vazio para que no zero não imprima nada
 
 for (let lineIndex = 0; lineIndex <= size; lineIndex += 1) { // For para percorrer de zero até size
   console.log(inputLine); // A primeira impressão que é uma string vazia
-  inputLine = inputLine + symbol; /* O acréscimo do esteristico na para que, na próxima vez que imprimir, a inputLine tenha um a mais, formando um triângulo. */
+  inputLine = inputLine + symbol; /* O acréscimo do esterisco na linha para que, na próxima vez que imprimir, a inputLine tenha um a mais, formando um triângulo. */
 };
 
 function formaDois(n) {
@@ -94,7 +94,7 @@ function formaTres(n) {
   const simbolo = '*';
   let linha = '';
   let coluna = n;
-  for (let line = 0; line <= n; line += 1) {
+  for (let line = 0; line < n; line += 1) {
     for (let column = 0; column <= n; column += 1) {
       if (column < coluna) {
         linha += ' ';
@@ -106,4 +106,148 @@ function formaTres(n) {
       coluna -= 1;
   } 
 }
-formaTres(6);
+formaTres(5);
+
+/* Depois, faça uma pirâmide com n asteriscos de base. Exemplo:
+n = 5
+
+  *
+ ***
+*****
+
+É importante notar que, diferente dos casos anteriores, onde havia sequencias de espaços em branco e de asteristicos uma após as outras, o que marca a construção dessa pirâmide é o asterístico do meio, sempre presente, e o fato de que é a partir dele que vão ser colocados asteristicos a esquerda e a direita para formar a pirâmide.
+*/
+
+
+let n = 5; // Variável para a base, que será o número oferecido
+let symbol = '*'; // O que será usado para construir o triângulo
+let inputLine = ''; // Variável que vai armazenar a linha
+
+let midOfMatrix = (n + 1) / 2; /* Variável que guarda o meio da pirâmide, que é justamente a posição correspondente ao número de asteristicos na base mais 1 (n + 1) dividido ao meio (n + 1) / 2. */
+let controlLeft = midOfMatrix; // Variável para o lado esquerdo do centro da pirâmide
+let controlRight = midOfMatrix; // Variável para o lado direito do centro da pirâmide
+
+for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) { /*For para percorrer de 0 até o meio da pirâmide, já que agora o númerode linhas, de acordo com o exemplo, corresponde não ao n oferecido, mas ao midOfMatrix */
+  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) { // For para percorrer de 0 a n para PREENCHER cada posição (coluna) da linha
+    if (columnIndex > controlLeft && columnIndex < controlRight) { /* Condicional para verificar se o columnIndex está na posição correta para ser preenchida com asteristico, ou seja, maior que a margem de controle esquerda e menor que a margem de controle direita; como controlLeft será decrecido e controlRight será acrescido a cada rodada do for, será incluído um asteristico a mais de cada aldo. */
+      inputLine = inputLine + symbol; /* O que será executado na condicional, que é incluir na linha, na posição (columnIndex) que entrou no if um asteristico */
+    } else { /* Será executado sempre que a columnIndex não entrar no if, o que corresponde as vezes em que ela não forma um triângulo partindo do meio da pirâmide */
+      inputLine = inputLine + ' '; // Adiciona naquela posição do columnIndex um espaço
+    }
+  }
+  console.log(inputLine); // Imprime a linha
+  inputLine = ''; // Esvazia a linha para que ela seja refeita quando o for rodar novamente
+  controlRight += 1; // Acrescenta um ao lado direito da pirâmide
+  controlLeft -= 1 // Retira um do lado esquerdo da pirâmide
+};
+
+function formaQuatro(n) {
+  const simbolo = '*';
+  let linha = '';
+  const meio = (n + 1) / 2;
+  let ladoEsquerdo = meio;
+  let ladoDireito = meio;
+  for (let l = 0; l <= meio; l += 1) {
+    for (let c = 0; c <= n; c += 1) {
+      if (c > ladoEsquerdo && c < ladoDireito) {
+        linha += simbolo;
+      } else {
+        linha += ' ';
+      }
+    } console.log(linha);
+      linha = '';
+      ladoEsquerdo -= 1;
+      ladoDireito += 1;
+  }
+}
+formaQuatro(5);
+
+/* Faça uma pirâmide com n asteriscos de base, que seja vazia no meio.Assuma que o valor de n será sempre ímpar:
+
+Por último, façamos com que a variável seja incrementada com o valor correspondente a cada loop;
+n = 7
+
+   *
+  * *
+ *   *
+*******
+
+*/
+
+let n = 7; // Variável para a base, que será o número oferecido
+let middle = (n + 1) / 2; /* Variável que guarda o meio da pirâmide, que é justamente a posição correspondente ao número de asteristicos na base mais 1 (n + 1) dividido ao meio (n + 1) / 2. */
+let controlLeft = middle; // Variável para o lado esquerdo do centro da pirâmide
+let controlRight = middle; // Variável para o lado direito do centro da pirâmide
+let symbol = '*'; // O que será usado para construir o triângulo
+for (let line = 1; line <= middle; line += 1) { /*For para percorrer de 0 até o meio da pirâmide, já que agora o númerode linhas, de acordo com o exemplo, corresponde não ao n oferecido, mas ao middle */
+  let outputLine = ''; // Variável que armazena linha, declarada aqui para ser reiniciada a cada for de linha
+  for (let col = 1; col <= n; col += 1) { // For para percorrer de 1 a n para PREENCHER cada posição (coluna) da linha
+    if (col == controlLeft || col == controlRight || line == middle) { /* Condicional para garantir que o triângulo seja vazio no meio; o for só entrará nesse if se a posição corresponder ao controle esquerdo, ao controle direito ou se for a última linha */
+      outputLine += symbol; /* O que será executado na condicional, que é incluir na linha, na posição (col) que entrou no if um asteristico */
+    } else { /* Será executado sempre que a col não entrar no if, o que corresponde as vezes em que ela não forma um triângulo partindo do meio da pirâmide ou for um preenchimento de dentro dela. */
+      outputLine += ' '; // Adiciona naquela posição do col um espaço
+    }
+  }
+  controlLeft -= 1; // Decrementa lado esquerdo
+  controlRight += 1; // Acrescenta lado direito
+  console.log(outputLine); // Imprime a linha
+}
+  
+function formaCinco(n) {
+  let numero = n;
+  if (n % 2 === 0) {
+    numero = n - 1
+  } 
+  const meio = (numero + 1) / 2;
+  let ladoDireito = meio;
+  let ladoEsquerdo = meio;
+  const simbolo = '*';
+  for (let l = 1; l <= meio; l += 1) {
+    let linha = '';
+    for (let c = 1; c <= numero; c += 1) {
+      if (c === ladoDireito || c === ladoEsquerdo || l === meio) {
+        linha += simbolo;
+      } else {
+        linha += ' ';
+      }
+    } ladoDireito += 1;
+      ladoEsquerdo -= 1;
+      console.log(linha);
+  }
+}
+formaCinco(8);
+
+/* 6- Faça um programa que diz se um número definido numa variável é primo ou não.
+
+==> Um número primo é um número que só é divisível por 1 e por ele mesmo, ou seja, a divisão dele com quaisquer outros números dá resto diferente de zero.
+==> Dica: você vai precisar de fazer um loop que vá de 0 ao número definido; Além disso, vai precisar de fazer uma checagem a cada iteração e armazenar os resultados em algum lugar.
+*/
+let numero = 3; // Variável para armazenar o número que será verificado
+
+let divisores = []; // Array pa guardar os divisores
+
+for (let n = 1; n <= numero; n += 1) { // For para percorrer todos os números de um até o número verificado
+  if (numero % n === 0) { // Condicional para verificar se o número é divisível por n (ou seja,se o resto de sua divisão é igual a 0)
+    divisores.push(n); // Inclui n no array de ele for um divisor válido
+  }
+}
+if (divisores.length > 2) {
+  console.log('Esse número não é primo');
+} else {
+  console.log('Esse número é primo');
+}
+
+function ePrimo(n) {
+  let divisores = [];
+  for (let num = 1; num <= n; num += 1) {
+    if (n % num === 0) {
+      divisores.push(num)
+    }
+  } if (divisores.length < 2) {
+    console.log('Esse número não é primo');
+    } else {
+  console.log('Esse número é primo');
+    }
+}
+
+ePrimo(15);
