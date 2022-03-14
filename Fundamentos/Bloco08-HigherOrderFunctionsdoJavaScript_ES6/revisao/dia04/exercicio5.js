5b77b2fe68a088ee59d411e5c181650f1d58fa24
Airel <mv.ribeirosilva@gmail.com>
2022-03-14 00:31:36 -0300

    Exercicios do dia 8.3, do 1 ao 4

---
+++ Fundamentos/Bloco08-HigherOrderFunctionsdoJavaScript_ES6/revisao/dia04/exercicio5.js
@@ -0,0 +1,11 @@
+// 5 - Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
+
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+function containsA() {
+  // escreva seu código aqui
+}
