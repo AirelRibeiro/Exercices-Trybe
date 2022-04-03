5c35987f377395664d9cab069004d35dcae84014
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-04-02 23:52:06 -0300

    Fixacao e exercicios de 10.1

---
+++ Front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/fixando/src/Component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Component extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Airel Ribeiro</h1>
+        <p>lorem ipsum dolor sit amet consectetur adipiscing elit aliquam volutpat auctor vulputate morbi vel platea augue ultricies mollis eget nisi faucibus torquent nec laoreet ut facilisi accumsan odio vivamus ultrices scelerisque himenaeos tortor inceptos potenti sodales hac quisque sem at urna tempor rutrum venenatis congue taciti ullamcorper cursus facilisis ligula aptent convallis risus iaculis pharetra pellentesque suscipit finibus litora a nascetur blandit primis pulvinar sed suspendisse habitasse aenean vehicula curae in pretium dignissim fringilla eleifend felis molestie euismod dis parturient maximus natoque duis libero viverra ornare interdum orci quis condimentum tellus dictumst penatibus nulla nostra gravida magnis conubia proin tristique</p>
+      </div>
+    )
+  }
+}
+
+export default Component;
---
+++ Front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/testando-npm-npx/testando-meu-computador/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Component from './Componente';
+
+function fixando() {
+  const textJSX = <h1>Hello JSX!</h1>;
+  return textJSX;
+}
+
+function App() {
+  return (
+    <Component/>
+  );
+}
+
+export default App;
