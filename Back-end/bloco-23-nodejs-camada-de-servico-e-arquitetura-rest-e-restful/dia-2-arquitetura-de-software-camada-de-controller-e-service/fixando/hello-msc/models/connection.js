595144702755b25a6e8a815db718f6c7d315cc8d
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-08 19:39:50 -0300

    feat: inclui exercício de fixação (Praticando)

---
+++ Back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/fixando/hello-msc/models/connection.js
@@ -0,0 +1,12 @@
+// hello-msc/models/connection.js
+
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'sua_senha',
+  database: 'model_example',
+  });
+
+module.exports = connection;
