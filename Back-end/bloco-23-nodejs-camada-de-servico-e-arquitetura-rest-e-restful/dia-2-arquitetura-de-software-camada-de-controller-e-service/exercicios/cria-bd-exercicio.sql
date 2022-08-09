1c113443c02dd71e256e292406ea80956eff5fed
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-08 23:13:38 -0300

    feat: cria data base e popula tabela ceps

---
+++ Back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercicios/cria-bd-exercicio.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS cep_lookup;
+
+USE cep_lookup;
+
+CREATE TABLE IF NOT EXISTS ceps (
+  cep VARCHAR(8) NOT NULL,
+  logradouro VARCHAR(50) NOT NULL,
+  bairro VARCHAR(20) NOT NULL,
+  localidade VARCHAR(20) NOT NULL,
+  uf VARCHAR(2) NOT NULL,
+  PRIMARY KEY (cep)
+);
