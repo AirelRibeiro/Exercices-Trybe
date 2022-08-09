c00d2501c007bf1fe1f36f8d58039e1133ce3df7
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-09 12:51:44 -0300

    feat: inclui insertCepInDataBase

--- Back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercicios/cep-lookup/controllers/CepController.js
+++ Back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercicios/cep-lookup/controllers/CepController.js
@@ -1,7 +1,6 @@
 const CepService = require('../services/CepService');
 
 function formatAdress(adress) {
-  console.log('Cheguei', adress);
   const CEP_REGEX = /^\d{5}-\d{3}$/;
   let cep = CEP_REGEX.test(adress.cep) ? adress.cep : adress.cep.replace(/(\d{5})(\d{3})/, '$1-$2');
   return { cep, ...adress };
@@ -12,6 +11,12 @@ async function findAdressByCep(cep) {
   return formatAdress(adress[0]);
 }
 
+async function insertCepInDataBase(cep, logradouro, bairro, localidade) {
+  const Cep = CepService.insertCepAdress(cep, logradouro, bairro, localidade);
+  return Cep;
+}
+
 module.exports = {
-  findAdressByCep
+  findAdressByCep,
+  insertCepInDataBase
 }
