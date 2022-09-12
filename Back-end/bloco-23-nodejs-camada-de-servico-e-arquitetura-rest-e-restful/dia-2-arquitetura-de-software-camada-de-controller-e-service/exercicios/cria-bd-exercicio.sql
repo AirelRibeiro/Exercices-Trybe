51a8f4505ba862641f6492b6eb95193bb5a39384
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-12 15:54:25 -0300

    feat: inclusão de query para exercícios do bloco 23

--- Back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercicios/cria-bd-exercicio.sql
+++ Back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercicios/cria-bd-exercicio.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE IF NOT EXISTS cep_lookup;
+CREATE DATABASE IF NOT EXISTS cep_lookup_dois;
 
-USE cep_lookup;
+USE cep_lookup_dois;
 
 CREATE TABLE IF NOT EXISTS ceps (
   cep VARCHAR(8) NOT NULL,
@@ -10,3 +10,15 @@ CREATE TABLE IF NOT EXISTS ceps (
   uf VARCHAR(2) NOT NULL,
   PRIMARY KEY (cep)
 );
+
+INSERT INTO ceps (cep, logradouro, bairro, localidade, uf)
+VALUES
+	('12345678', 'Rua feliz', 'Casinha', 'Aqui', 'AB'),
+	('91123456', 'Rua triste', 'Casona', 'Lá', 'BC'),
+	('78901234', 'Rua média', 'Da minha casa', 'Acolá', 'DE'),
+	('56789012', 'Rua nem lá nem cá', 'Vamos todos juntos', 'Logo ali', 'EF'),
+	('34567890', 'Rua psicótica', 'Bairro do Rivotril', 'Já chegamos', 'FG'),
+	('41424344', 'Rua duvidosa', 'Homem hétero branco', 'Ali no Facebook', 'GH'),
+	('45464748', 'Rua ladrilhada', 'Não vamos não', 'Para meu amor passar', 'HI');
+    
+    
