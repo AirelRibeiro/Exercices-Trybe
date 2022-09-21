17c2711819fccb964f652cdf0c7f7cdf2e3b6572
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-09-21 19:28:56 -0300

    feat: coloca servidor para rodar na porta 3000

--- Back-end/bloco-27-programacao-orientada-a-objetos/dia-4-solid-introducao-principios-s-o-d/exercicios/src/index.ts
+++ Back-end/bloco-27-programacao-orientada-a-objetos/dia-4-solid-introducao-principios-s-o-d/exercicios/src/index.ts
@@ -7,3 +7,8 @@ app.use(express.json());
 const endpointPlatWithId = '/plant/:id';
 
 const database = new PlantDatabase();
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(
+  `Server is running on PORT: ${PORT}`,
+));
